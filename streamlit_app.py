a9a609ccedc747c9797a0057200c5c3a0714f93d
wkomenda <113391427+wkomenda@users.noreply.github.com>
2022-09-14 12:58:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import requests
 import snowflake.connector
 import pandas
 from urllib.error import URLerror
@@ -27,7 +28,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
