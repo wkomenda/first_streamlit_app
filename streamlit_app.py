41731bb40b3dd3d31ed97978f97fba61256363cd
wkomenda <113391427+wkomenda@users.noreply.github.com>
2022-09-14 12:33:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 streamlit.text("What fruit would you like to add?")
 fruit_addition = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_addition)
